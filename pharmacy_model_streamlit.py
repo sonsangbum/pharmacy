ed9fb9860527acb835cb575598b3c638097eaa94
sonsangbum <sonsangbum@gmail.com>
2024-08-06 13:28:35 +0900

    Add files via upload

--- pharmacy_model_streamlit.py
+++ pharmacy_model_streamlit.py
@@ -105,7 +105,7 @@ if user_id=='phar' and user_password == "1234" :
   val_year = st.number_input("1.년도 입력(예: 2024년->2024)", value=today.year) 
   val_month = st.number_input("2.월 입력(예: 8월->8)", value=today.month)  
   val_weekday = st.number_input("3.요일 입력(예:월:0,화:1,수:2,목:3,금:4,토:5)", min_value=0, max_value=5, step=1, format="%d")
-  val_SC= st.number_input("4.예상 일조량을 입력(맑음:0,구름조금:25,구름많음:50,흐림:75)", min_value=0, max_value=5, step=1, format="%d")
+  val_SC= st.number_input("4.예상 일조량을 입력(맑음:0,구름조금:25,구름많음:50,흐림:75)", min_value=0, max_value=100, step=1, format="%d")
   val_mintemp= st.number_input("5.예상 최저온도를 입력")
   val_rainfall= st.number_input("6.예상 강수량(mm)을 입력")
   val_maxwindspeed= st.number_input("7.예상 최대풍속을 입력")
